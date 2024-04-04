e286d439ec7621d88cbbac1136a49e9c52038417
ksboo <136306422+ksboo@users.noreply.github.com>
2024-04-04 20:50:00 +0530

    Add files via upload

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/khushbooshrivastava/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/khushbooshrivastava/Desktop/Flutter/Airit/Test_Codes/live_system"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/khushbooshrivastava/Desktop/Flutter/Airit/Test_Codes/live_system/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC80YTU4NWI3OTI5NGU4MzBmYTg5YzI0OTI0ZDU4YTI3Y2M4ZmJmNDA2Lw=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/khushbooshrivastava/Desktop/Flutter/Airit/Test_Codes/live_system/.dart_tool/package_config.json"
